6f1dfda6d41da2b39b25089f7fce0f45561cde92
MAllchin8 <74734264+MAllchin8@users.noreply.github.com>
2021-03-13 15:29:09 +0000

    New reduce.py

--- reduce.py
+++ reduce.py
@@ -5,7 +5,7 @@ popularProducts = [[]]
 popularProducts[0].append("water bottle")
 popularProducts[0].append("instead buying one use plastic bottle buy a reusable water bottle if you are opposed to " +
                           "tap water buy a large bottle of water, or a water filteration system you can use to refill bottle")
-popularProducts[0].append("doing this can reduce up to 156 bottles per year")    
+popularProducts[0].append("doing this can reduce up to 156 bottles per year")
 
 
 #bumboo toilet paper made of bamboo
@@ -15,9 +15,10 @@ popularProducts[0].append("doing this can reduce up to 156 bottles per year")
 #in China, most of the bamboo is grown by rural smallholders and administrated through co-operatives, thus supporting rural development and giving farmers a sustainable income.
 popularProducts.append([])
 popularProducts[1].append("Toilet paper")
-popularProducts[1].append("Instead of using toilet papers from trees use bamboo toilet paper such as bumboo or even better use a bidet to totally reduce this source of paper watse")
+popularProducts[1].append(
+    "Instead of using toilet papers from trees use bamboo toilet paper such as bumboo or even better use a bidet to totally reduce this source of paper watse")
 popularProducts[1].append("3.7 gallons of water a day can be saved using a bidet. bamboo is the fastest growing plant and only takes 3-5 years to mature while tre" +
-                          "es can take 25-40 years to replace. it also supports rural development and giving farmers a sustainable income")  
+                          "es can take 25-40 years to replace. it also supports rural development and giving farmers a sustainable income")
 
 popularProducts.append([])
 popularProducts[2].append("Clothing")
@@ -33,57 +34,65 @@ popularProducts[3].append("reduces the number of plastic waste produced")
 
 popularProducts.append([])
 popularProducts[4].append("single use cleaning products")
-popularProducts[4].append("instead of buying single use cleaning products the throwing out the plastic bottle "+
+popularProducts[4].append("instead of buying single use cleaning products the throwing out the plastic bottle " +
                           "buy cleaning tablets which you just add water and resuse a glass container to store the cleaning products. An example is Blueland")
 popularProducts[4].append("reduces the number of plastic waste produced")
 
 popularProducts.append([])
 popularProducts[5].append("New things")
-popularProducts[5].append("Instead of buying new things consider the items you already own in your home. Think of whether you really need that item.")
-popularProducts[5].append("Reduces the energy used to build those items and Co2 to ship them, ensures old items dont go to waste")
+popularProducts[5].append(
+    "Instead of buying new things consider the items you already own in your home. Think of whether you really need that item.")
+popularProducts[5].append(
+    "Reduces the energy used to build those items and Co2 to ship them, ensures old items dont go to waste")
 
 popularProducts.append([])
 popularProducts[6].append("food")
-popularProducts[6].append("compost your own food watse and use it as a fertilizer")
-popularProducts[6].append("reduces food waste sent to a landfill, ensures food waste is properly used")
+popularProducts[6].append(
+    "compost your own food watse and use it as a fertilizer")
+popularProducts[6].append(
+    "reduces food waste sent to a landfill, ensures food waste is properly used")
 
 popularProducts.append([])
 popularProducts[7].append("tea")
 popularProducts[7].append("drink loose leaf tea")
-popularProducts[7].append("Plastic accounts for roughly 25% of the teabag. By switching to loose leaf you will reduce the amount of plastic ending up in landfills")
+popularProducts[7].append(
+    "Plastic accounts for roughly 25% of the teabag. By switching to loose leaf you will reduce the amount of plastic ending up in landfills")
 
-popularProducts.append([])
-popularProducts[8].append("")
-popularProducts[8].append("")
-popularProducts[8].append("")
+#popularProducts.append([])
+#popularProducts[8].append("")
+#popularProducts[8].append("")
+#popularProducts[8].append("")
 
-popularProducts.append([])
-popularProducts[9].append("")
-popularProducts[9].append("")
-popularProducts[9].append("")
+#popularProducts.append([])
+#popularProducts[9].append("")
+#popularProducts[9].append("")
+#popularProducts[9].append("")
 
 
 def displayALternativesAndInfo():
     return popularProducts
 
+
 def returnArray(Item):
     if (Item == "water bottle"):
-        return popular[0]
-    else if (Item == "Toilet paper"):
-        return popular[1]
-    else if (Item == "Clothing"):
-        return popular[2]
-    else if (Item == "single use plastic packaging"):
-        return popular[3]
-    else if (Item == "single use cleaning products"):
-        return popular[4]
-    else if (Item == "New things"):
-        return popular[5]
-    else if (Item == "food"):
-        return popular[6]
-    else if (Item == "tea"):
-        return popular[7]
+        return popularProducts[0]
+    elif (Item == "Toilet paper"):
+        return popularProducts[1]
+    elif (Item == "Clothing"):
+        return popularProducts[2]
+    elif (Item == "single use plastic packaging"):
+        return popularProducts[3]
+    elif (Item == "single use cleaning products"):
+        return popularProducts[4]
+    elif (Item == "New things"):
+        return popularProducts[5]
+    elif (Item == "food"):
+        return popularProducts[6]
+    elif (Item == "tea"):
+        return popularProducts[7]
+
 
 if __name__ == "__main__":
-    
+
     print(displayALternativesAndInfo())
+    print(returnArray("tea"))
