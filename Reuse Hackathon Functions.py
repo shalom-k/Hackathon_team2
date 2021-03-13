7a9cd306f26ae3e536087ae3fc1c8faa46c43d1f
CallumW2001 <46678107+CallumW2001@users.noreply.github.com>
2021-03-13 15:23:25 +0000

    Add files via upload

--- Reuse Hackathon Functions.py
+++ Reuse Hackathon Functions.py
@@ -1,38 +1,55 @@
 #12/03/2021
-#Written By: Callum Wilkinson
 #Reuse
 
 import sqlite3
+import requests,json
+import openpyxl
 
-#file = database
+excel_files = ['HackLboro_-_Team_2_-_Database.xlsx']
 
-#engine = create_engine('sqlite://', echo = False)
+for file in excel_files:
 
-activ = open("activities.txt", "r");
+    workbook = openpyxl.load_workbook(file);
+    worksheet = workbook['Activities']
+
+
 
 def displayReuseMethod(material):
 
     activitytext = [];
-    activitytext = activ.readlines();
 
     activities = [];
     activity = "";
+
     
-    if(material[0].lower() == "wood"):
+    if(material.lower() == "scrap wood"):
 
-        activities.append(activitytext[0]);
-        
-    elif(material[0].lower() == "glue"):
+        activities.append(worksheet['B2'].value);
 
-        activites.append(activitytext[1]);
+    elif(material.lower() == "glue"):
 
-    elif(material[0].lower() == "plastic"):
+        activities.append(worksheet['B3'].value);
+                
+    elif(material.lower() == "paper"):
 
-        activities.append(...);
+        activities.append(worksheet['B4'].value);
 
-    elif(material[0].lower() == "metal"):
+    elif(material.lower() == "cardboard"):
 
-        activites.append(...);
+        activities.append(worksheet['B5'].value);
+
+    elif(material.lower() == "metal"):
+
+        activities.append(worksheet['B6'].value);
+
+    elif(material.lower() == "plastic bottles"):
+
+        activities.append(worksheet['B7'].value);
+
+    elif(material.lower() == "textiles"):
+
+        activities.append(worksheet['B8'].value);
+
 
         
 
@@ -46,21 +63,52 @@ def displayReuseMethod(material):
     
 
     return activity;
+    
+def nearby_locations(place):
+    
+    api_key = 'AIzaSyBbwM-62klXAknNAhMWEZ-MVlpfUFYFYko'
+    url_place = "https://maps.googleapis.com/maps/api/place/textsearch/json?"
+    r = requests.get(url_place + 'query=' + place +'&key=' + api_key)
+    x = r.json()
+    data = x['results'] 
+    return(x['results'])
+
+def distance(start,fin):
+
+    api_key = 'AIzaSyBbwM-62klXAknNAhMWEZ-MVlpfUFYFYko'
+    url_distance='https://maps.googleapis.com/maps/api/distancematrix/json?'
+    req = requests.get(url_distance + 'origins=' + start + '&destinations=' + fin +'&key=' + api_key)
+    values=req.json()
+    #can change this print statement to a return statement to release the data onto GUI
+    print("%s: %s to %s: %s is %s away and would take %s"%(values['destination_addresses'][0],fin,values['origin_addresses'][0],start,values['rows'][0]['elements'][0]['distance']['text'],values['rows'][0]['elements'][0]['duration']['text']))
+    return("%s: %s to %s: %s is %s away and would take %s"%(values['destination_addresses'][0],fin,values['origin_addresses'][0],start,values['rows'][0]['elements'][0]['distance']['text'],values['rows'][0]['elements'][0]['duration']['text']))
+
+def locateNearbyCharities():
+    
+    place = ("Charity Near Me")
+
+
+    charities = "";
+
+    x = nearby_locations(place)
+    charitylist = []
+    dlist = []
+    
+    for i in range(len(x)):
+        charitylist.append(x[i]['name'])
+
+    print(charitylist)
+
+    start = input("Enter Your Postcode : ")
+
+    for l in range(len(charitylist)):
+
+        dlist.append(distance(start,charitylist[l]));
 
     
 
+    return charitylist;
 
-"""def locateNearbyCharities():
-
-    str charitynames = [];
-
+if __name__ == "__main__":
     
-
-
-
-    return charitynames;
-
-
-"""
-
-displayReuseMethod(["wood"]);
+    locateNearbyCharities();
